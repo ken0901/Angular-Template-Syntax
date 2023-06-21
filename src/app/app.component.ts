81848f7749ecd3298957276cccc9d34492b1b027
ken0901 <ken5260715@gmail.com>
2023-06-20 22:31:24 -0700

    .gitignore is now working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  onButtonClick(){
+    console.log('button was clicked');
+  }
 }
