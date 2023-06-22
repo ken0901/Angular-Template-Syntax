ea9047454d8e8d8c47bc4f953d213dd1abaee7c4
ken0901 <ken5260715@gmail.com>
2023-06-21 21:35:17 -0700

    Tracking Input state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  includeLetters = false;
+  includedNumber = false;
+  includeUseSymbols = false;
+  password = '';
+
   onButtonClick(){
-    console.log('button was clicked');
+    console.log(this.includeLetters);
+    console.log(this.includedNumber);
+    console.log(this.includeUseSymbols);
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumber(){
+    this.includedNumber = !this.includedNumber;
+  }
+
+  onChangeUseSymbols(){
+    this.includeUseSymbols = !this.includeUseSymbols;
   }
 }
