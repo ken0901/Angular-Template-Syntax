3f5e51c6a432f8439336b8f67d8b87fa841534e4
ken0901 <ken5260715@gmail.com>
2023-06-21 22:10:59 -0700

    Generating a random password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0;
   includeLetters = false;
   includedNumber = false;
   includeUseSymbols = false;
   password = '';
 
+  onChangeLength(event: Event){
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
   onButtonClick(){
-    console.log(this.includeLetters);
-    console.log(this.includedNumber);
-    console.log(this.includeUseSymbols);
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%$%^&*()';
+
+    let validChars = '';
+    if(this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includedNumber){
+      validChars += numbers;
+    }
+    if(this.includeUseSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 
   onChangeUseLetters(){
     this.includeLetters = !this.includeLetters;
   }
 
-  onChangeUseNumber(){
+  onChangeUseNumbers(){
     this.includedNumber = !this.includedNumber;
   }
 
