035a4fabe50f190ccec3dcdc6006cb2b657ea751
ken0901 <ken5260715@gmail.com>
2023-06-23 21:45:01 -0700

    Accepting data in a child component

--- src/app/projects/cards/card/card.component.ts
+++ src/app/projects/cards/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'card',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  @Input() title;
+  @Input() imageUrl;
+  @Input() username;
+  @Input() content;
 
   constructor() { }
 
