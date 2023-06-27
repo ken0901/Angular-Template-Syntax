da1390d9ba051e607e71fb4ed11899f019b36a79
ken0901 <ken5260715@gmail.com>
2023-06-26 22:01:25 -0700

    Character,Styling by Comparison

--- src/app/projects/typing-challenge/typing-challenge.component.ts
+++ src/app/projects/typing-challenge/typing-challenge.component.ts
@@ -7,10 +7,41 @@ import { faker } from "@faker-js/faker";
   styleUrls: ['./typing-challenge.component.css']
 })
 export class TypingChallengeComponent implements OnInit {
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+  
+  // other way
+  // solved = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onInput(event: Event){
+    const target = event.target as HTMLInputElement;
+    this.enteredText = target.value;
+
+    /*
+    * other way
+    * It can't be compared each character that is wrong.
+    * 
+    if(target.value === this.randomText){
+      this.solved = true;
+    }
+    */
+  }
+
+  compare(randomLetter: string, enteredLetter: string){
+    if(!enteredLetter){
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct': 'incorrect';
+    // if(enteredLetter === randomLetter){
+    //   return 'correct';
+    // }else {
+    // return 'incorrect';
+    //}
+  }
 }
