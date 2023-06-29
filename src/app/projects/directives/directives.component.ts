5f727f10fd5993a4bf9a9596f52267527a41aa8d
ken0901 <ken5260715@gmail.com>
2023-06-28 21:28:31 -0700

    The ngClass Directive

--- src/app/projects/directives/directives.component.ts
+++ src/app/projects/directives/directives.component.ts
@@ -6,11 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directives.component.css']
 })
 export class DirectivesComponent implements OnInit {
+  currentPage = 0;
   images = [
     {
       title: 'At the Beach',
       url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-    }
+    },
+    {
+      title: 'At the Gorest',
+      url: 'https://images.unsplash.com/photo-1425913397330-cf8af2ff40a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9yZXN0fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'At the City',
+      url: 'https://images.unsplash.com/photo-1444723121867-7a241cacace9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Q2l0eXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'At the Snow',
+      url: 'https://images.unsplash.com/photo-1551582045-6ec9c11d8697?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fFNub3d8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+    },
   ];
 
   constructor() { }
