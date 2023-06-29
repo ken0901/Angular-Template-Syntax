1f4dffe98339bfd5e2fbabad6491ed9efac2f2c6
ken0901 <ken5260715@gmail.com>
2023-06-28 22:03:23 -0700

    Communicating properties to directives

--- src/app/projects/directives/class.directive.ts
+++ src/app/projects/directives/class.directive.ts
@@ -1,12 +1,34 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
+  @Input() backgroundColor: string;
 
   constructor(private element: ElementRef) {
-    this.element.nativeElement.style.backgroundColor = 'orange';
+    this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    
+    // NEVER DO THIS !!!
+    // setTimeout(() => {
+    //   this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    // }, 50);
    }
 
 }
+
+
+
+
+
+
+// Communicate from parent to child component...
+
+// Parent component template
+// <app-card [title]= " 'SnowyMountains' "></app-card>
+
+// Child component class
+// import {Input} from 'angular'
+// class ChildComponent{
+//   @Input() title: string;
+// }
