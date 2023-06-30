90c97c3699f97faf7c1a75ada7b4f6822156d942
ken0901 <ken5260715@gmail.com>
2023-06-29 21:12:13 -0700

    Context in Structrual Directives

--- src/app/projects/directives/times.directive.ts
+++ src/app/projects/directives/times.directive.ts
@@ -13,7 +13,9 @@ export class TimesDirective {
     this.viewContainer.clear();
 
     for(let i=0; i<times; i++){
-      this.viewContainer.createEmbeddedView(this.templateRef, {});
+      this.viewContainer.createEmbeddedView(this.templateRef, {
+        index: i
+      });
     }
   }
 }
