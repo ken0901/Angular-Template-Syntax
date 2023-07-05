5f87dac55addfd5dd53c55440ccdae7d31d9d9ed
ken0901 <ken5260715@gmail.com>
2023-07-04 20:38:16 -0700

    Communicating Down Table Data

--- src/app/projects/module-project/components/collections-home/collections-home.component.ts
+++ src/app/projects/module-project/components/collections-home/collections-home.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'Designer'},
+    { name: 'Ken', age: 32, job: 'Developer'},
+    { name: 'Kaite', age: 30, job: 'Developer'},
+  ]
+
+  headers = [
+    { key: 'name', label: 'Name'},
+    { key: 'age', label: 'Age'},
+    { key: 'job', label: 'Job'},
+  ]
 
   constructor() { }
 
