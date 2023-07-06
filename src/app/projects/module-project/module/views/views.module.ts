9479931b60e1d3abfe104baed896e86b8e288851
ken0901 <ken5260715@gmail.com>
2023-07-05 21:41:04 -0700

    Displaying a list of statistics

--- src/app/projects/module-project/module/views/views.module.ts
+++ src/app/projects/module-project/module/views/views.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from '../../components/views-home/views-home.component';
+import { StatisticsComponent } from '../../components/views/statistics/statistics.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    ViewsHomeComponent
+    ViewsHomeComponent,
+    StatisticsComponent
   ],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ]
 })
 export class ViewsModule { }
