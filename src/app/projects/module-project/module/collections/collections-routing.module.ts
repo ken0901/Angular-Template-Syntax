0eb4bc52125ad73cbc8875d27ea97ed07dd13dd4
ken0901 <ken5260715@gmail.com>
2023-07-05 21:04:12 -0700

    Adding Child Navigation Routes

--- src/app/projects/module-project/module/collections/collections-routing.module.ts
+++ src/app/projects/module-project/module/collections/collections-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionsHomeComponent } from '../../components/collections-home/collections-home.component';
+import { BiographyComponent } from '../../components/collections/biography/biography.component';
+import { CompaniesComponent } from '../../components/collections/companies/companies.component';
+import { PartnersComponent } from '../../components/collections/partners/partners.component';
 
 const routes: Routes = [
-  { path: '', component:CollectionsHomeComponent}
+  { 
+    path: '', 
+    component:CollectionsHomeComponent,
+    children: [
+      { path: '', component: BiographyComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'partners', component: PartnersComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/projects/module-project/module/collections/collections.module.ts
+++ src/app/projects/module-project/module/collections/collections.module.ts
@@ -5,12 +5,18 @@ import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from '../../components/collections-home/collections-home.component';
 import { TableComponent } from '../../components/table/table.component';
 import { SharedModule } from '../../shared/shared.module';
+import { BiographyComponent } from '../../components/collections/biography/biography.component';
+import { CompaniesComponent } from '../../components/collections/companies/companies.component';
+import { PartnersComponent } from '../../components/collections/partners/partners.component';
 
 
 @NgModule({
   declarations: [
     CollectionsHomeComponent,
-    TableComponent
+    TableComponent,
+    BiographyComponent,
+    CompaniesComponent,
+    PartnersComponent
   ],
   imports: [
     CommonModule,
