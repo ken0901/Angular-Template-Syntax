c5ef4f05337f7770eaf96c910400a58a31628054
ken0901 <ken5260715@gmail.com>
2023-07-06 21:06:45 -0700

    Solving the Modal Issue

--- src/app/projects/module-project/components/mods-home/mods-home.component.ts
+++ src/app/projects/module-project/components/mods-home/mods-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-mods-home',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
 }
