d71dbc8851d232aa7a31d22e730a757835bd3c60
ken0901 <ken5260715@gmail.com>
2023-07-06 22:08:22 -0700

    Listing Accordion Elements

--- src/app/projects/module-project/components/mods-home/mods-home.component.ts
+++ src/app/projects/module-project/components/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    { title: 'Why is the sky blue?', content: 'The sky is blue because it is'},
+    { title: 'What does an orange taste like?', content: 'An orange tastes like an orange'},
+    { title: 'What color is that cat?', content: 'The cat is an orange color'},
+  ];
 
   constructor() { }
 
--- src/app/projects/module-project/module/mods/mods.module.ts
+++ src/app/projects/module-project/module/mods/mods.module.ts
@@ -5,12 +5,14 @@ import { ModsRoutingModule } from './mods-routing.module';
 import { ModsHomeComponent } from '../../components/mods-home/mods-home.component';
 import { ModalComponent } from '../../components/mods/modal/modal.component';
 import { SharedModule } from '../../shared/shared.module';
+import { AccordionComponent } from '../../components/mods/accordion/accordion.component';
 
 
 @NgModule({
   declarations: [
     ModsHomeComponent,
-    ModalComponent
+    ModalComponent,
+    AccordionComponent
   ],
   imports: [
     CommonModule,
