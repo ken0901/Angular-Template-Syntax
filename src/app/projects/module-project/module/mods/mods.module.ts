0e7d5db44df18d08abf3fa6aed69be77bb9203ab
ken0901 <ken5260715@gmail.com>
2023-07-06 20:49:06 -0700

    Modal Window setup

--- src/app/projects/module-project/module/mods/mods.module.ts
+++ src/app/projects/module-project/module/mods/mods.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ModsRoutingModule } from './mods-routing.module';
 import { ModsHomeComponent } from '../../components/mods-home/mods-home.component';
+import { ModalComponent } from '../../components/mods/modal/modal.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    ModsHomeComponent
+    ModsHomeComponent,
+    ModalComponent
   ],
   imports: [
     CommonModule,
-    ModsRoutingModule
+    ModsRoutingModule,
+    SharedModule
   ]
 })
 export class ModsModule { }
