7ec03c2fdfa5c20622ae87bceccfec78de2ead1f
ken0901 <ken5260715@gmail.com>
2023-07-13 21:39:39 -0700

    Creating a Form Instance

--- src/app/projects/credit-card/module/credit-card/credit-card.module.ts
+++ src/app/projects/credit-card/module/credit-card/credit-card.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CardFormComponent } from '../../component/card-form/card-form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardFormComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    ReactiveFormsModule,
+    CardFormComponent
   ]
 })
 export class CreditCardModule { }
