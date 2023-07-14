158304bfd94d03fd87b4cb6d299fc557de872ff3
ken0901 <ken5260715@gmail.com>
2023-07-13 21:07:45 -0700

    HTTP dependency injection and making the request

--- src/app/projects/photos/service/photos.service.ts
+++ src/app/projects/photos/service/photos.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,13 @@ import { Injectable } from '@angular/core';
 })
 export class PhotosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID '
+      }
+    });
+  }
 }
