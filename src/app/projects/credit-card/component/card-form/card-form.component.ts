8016bc426747e5bd3892a52cf611a5f18fa2e0b5
ken0901 <ken5260715@gmail.com>
2023-07-13 22:02:15 -0700

    Showing and Hiding Validation Messages

--- src/app/projects/credit-card/component/card-form/card-form.component.ts
+++ src/app/projects/credit-card/component/card-form/card-form.component.ts
@@ -14,9 +14,25 @@ export class CardFormComponent implements OnInit {
     ])
   });
 
-  constructor() { }
+  constructor() {
+    console.log(this.cardForm.get('name'));
+   }
 
   ngOnInit(): void {
   }
 
 }
+
+/**
+ *  Validation - Form
+ * 
+ *  # Valid - Angular has validated whatever the user entered successfully
+ *  # Invalid - The value in the input is invalid
+ *  # Pending - Validation is currently running on this field
+ *  # Disabled - Ignore user input on this field and don't validate it
+ *  # touched - User clicked into then out of  a field
+ *  # Untouched - User hasn't clicked into then out of this field
+ *  # Pristine - User hasn't clicked on this field at all
+ *  # Dirty - User has changed the value of this field
+ * 
+ */
