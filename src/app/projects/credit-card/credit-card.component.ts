0a4a27b8a6ac57c08d2e9c1f3be6d53336e8c3d6
ken0901 <ken5260715@gmail.com>
2023-07-13 21:33:07 -0700

    Reactive Forms vs Template Forms

--- src/app/projects/credit-card/credit-card.component.ts
+++ src/app/projects/credit-card/credit-card.component.ts
@@ -16,7 +16,19 @@ export class CreditCardComponent implements OnInit {
 
 
 /**
+ * Reactive Forms vs Template Forms
  * 
  * 
+ * # Reactive Forms
+ *  Most of the form logic is driven by configuration in a component class file
+ *  More appropriate for complex forms 
+ *  Exposes some aspects of the form to us as RxJs Observables
+ *  We have to wire up the 'ReactiveFormsModule' to our app module to use them!
+ * 
+ * # Template Forms
+ *  Most of the form logic is driven by config in our component template file
+ *  More appropriate for simple forms
+ *  Harder to deal with dynamic forms (adding/removing form elements)
+ *  We have to wire up the 'FormsModule' to our App Module to use them !
  * 
  */
