fd64943379f0abb962b1bc126c09ac5a53e47e2f
ken0901 <ken5260715@gmail.com>
2023-07-14 21:58:17 -0700

    Reformatting the Input - Date

--- src/app/projects/credit-card/class/date-form-control.ts
+++ src/app/projects/credit-card/class/date-form-control.ts
@@ -2,7 +2,24 @@ import { FormControl } from "@angular/forms";
 
 export class DateFormControl extends FormControl{
     setValue(value: string, options: any) {
-        console.log(value);
+        if(value.match(/[^0-9|\/]/gi)){
+            super.value(this.value, {...options, emitModelToViewChange: true});
+            return;
+        }
+
+        if(value.length > 5) {
+            super.value(this.value, {...options, emitModelToViewChange: true});
+            return;
+        }
+
+        if(value.length === 2 && this.value.length === 3){
+            super.value(value, {...options, emitModelToViewChange: true});
+            return;
+        }
+
+        if(value.length === 2){
+            super.setValue(value + '/', {...options, emitModelToViewChange: true});
+        }
         super.setValue(value, options);
     }
 }
