9d5cd80750ee042dba05a0f1af7b56c4a1c1d680
ken0901 <ken5260715@gmail.com>
2023-07-14 22:45:10 -0700

    Adding a Credit Card

--- src/app/projects/credit-card/module/credit-card/credit-card.module.ts
+++ src/app/projects/credit-card/module/credit-card/credit-card.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CardFormComponent } from '../../component/card-form/card-form.component';
 import { InputComponent } from '../../component/input/input.component';
+import { CardComponent } from '../../component/card/card.component';
 
 @NgModule({
   declarations: [
     CardFormComponent,
     InputComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
@@ -17,6 +19,7 @@ import { InputComponent } from '../../component/input/input.component';
     ReactiveFormsModule,
     CardFormComponent,
     InputComponent,
+    CardComponent
   ]
 })
 export class CreditCardModule { }
