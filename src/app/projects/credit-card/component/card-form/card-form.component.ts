d677d0c93702be6fc4039b9044a4479158413c54
ken0901 <ken5260715@gmail.com>
2023-07-14 21:42:05 -0700

    Additional Validations

--- src/app/projects/credit-card/component/card-form/card-form.component.ts
+++ src/app/projects/credit-card/component/card-form/card-form.component.ts
@@ -9,9 +9,9 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [Validators.required,Validators.minLength(3),Validators.maxLength(5)]),
-    number: new FormControl(''),
-    expiration: new FormControl(''),
-    securityCode: new FormControl('')
+    number: new FormControl('', [Validators.required, Validators.minLength(16),Validators.maxLength(16)]),
+    expiration: new FormControl('', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]), // mm/dd
+    securityCode: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(3)])
   });
 
   constructor() {}
