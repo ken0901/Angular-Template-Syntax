988b5eea2874cd669aa06ec3305a0bf481a56b0a
ken0901 <ken5260715@gmail.com>
2023-07-19 21:35:23 -0700

    Detecting Changes and appyling a class name

--- src/app/projects/math-game/directives/answer-highlight.directive.ts
+++ src/app/projects/math-game/directives/answer-highlight.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef } from '@angular/core';
 import {NgControl} from '@angular/forms';
+import { filter, map } from 'rxjs/operators';
 
 @Directive({
   selector: '[appAnswerHighlight]'
@@ -9,6 +10,15 @@ export class AnswerHighlightDirective {
   constructor(private el: ElementRef, private controlName: NgControl) {}
 
   ngOnInit() {
-    console.log(this.controlName.control.parent);
+    this.controlName.control.parent.valueChanges
+      .pipe(
+        map(({ a, b, answer }) => Math.abs((a+b - answer) / (a+b))),
+      ).subscribe((value) => {
+        if (value < 0.2){
+          this.el.nativeElement.classList.add('close');
+        } else {
+          this.el.nativeElement.classList.remove('close');
+        }
+      });
   }
 }
