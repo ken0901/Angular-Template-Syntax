67f4e5788374d8eaf527f1de784b992ee73a55f9
ken0901 <ken5260715@gmail.com>
2023-07-20 21:04:59 -0700

    Username and Password Validation

--- src/app/projects/email-client/components/auth/signup/signup.component.ts
+++ src/app/projects/email-client/components/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,10 +7,28 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  /**
+   *  username - required - letter and numbers, min length 3, max length 20
+   *  password - required, min length 4, max length 20
+   *  passwordConfirmation - required, min length 4, max length 20
+   */
   authForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-    passwordConfirmation: new FormControl('')
+    username: new FormControl('', [
+      Validators.required, 
+      Validators.minLength(3), 
+      Validators.maxLength(20), 
+      Validators.pattern(/^[a-z0-9]+$/) // letter and number
+    ]),
+    password: new FormControl('', [
+      Validators.required, 
+      Validators.minLength(4), 
+      Validators.maxLength(20),
+    ]),
+    passwordConfirmation: new FormControl('', [
+      Validators.required, 
+      Validators.minLength(4), 
+      Validators.maxLength(20),
+    ])
   });
 
   constructor() { }
