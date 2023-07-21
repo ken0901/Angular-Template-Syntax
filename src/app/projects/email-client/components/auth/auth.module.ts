45a3bf0c96729a5824fa44c11b16583d72208de6
ken0901 <ken5260715@gmail.com>
2023-07-20 20:56:43 -0700

    Adding a Signup Form and styling

--- src/app/projects/email-client/components/auth/auth.module.ts
+++ src/app/projects/email-client/components/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,11 +14,8 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ],
-  exports: [
-    SigninComponent,
-    SignupComponent
-  ]
 })
 export class AuthModule { }
