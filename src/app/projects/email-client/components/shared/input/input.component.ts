041e4169cebb35c378696936035cf8a03357417a
ken0901 <ken5260715@gmail.com>
2023-07-22 20:15:59 -0700

    Robust Error Handling

--- src/app/projects/email-client/components/shared/input/input.component.ts
+++ src/app/projects/email-client/components/shared/input/input.component.ts
@@ -15,4 +15,8 @@ export class InputComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showErrors(){
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
 }
