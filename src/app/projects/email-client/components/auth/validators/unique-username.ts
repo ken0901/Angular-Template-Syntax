5d14eb78c4a1ce3c06fe8c341497ab16fda5e07f
ken0901 <ken5260715@gmail.com>
2023-07-22 19:49:09 -0700

    Handling Errors from Async Validation

--- src/app/projects/email-client/components/auth/validators/unique-username.ts
+++ src/app/projects/email-client/components/auth/validators/unique-username.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { AsyncValidator, FormControl } from "@angular/forms";
+import { of } from "rxjs";
+import { catchError, map } from "rxjs/operators";
 
 @Injectable({ providedIn:'root'})
 export class UniqueUsername implements AsyncValidator {
@@ -11,6 +13,20 @@ export class UniqueUsername implements AsyncValidator {
 
         return this.http.post<any>('https://api.angular-email.com/auth/username', {
             username: value
-        });
+        }).pipe(
+            map((value) =>{
+                if(value.available) // no nesscessary it is obvious available value.
+                    return null;
+            }),
+            catchError(err => {
+                console.log(err);
+                console.log(err.error.username);
+                if(err.error.username){
+                    return of( { nonUniqueUsername: true });
+                }else{
+                    return of({ noConnection: true });
+                }
+            })
+        );
     }
 }
