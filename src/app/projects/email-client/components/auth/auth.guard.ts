09cbe60d8ef1568ba26ebae47d7f01cdea0dcf80
ken0901 <ken5260715@gmail.com>
2023-07-26 21:32:29 -0700

    Issues with Guards and Solution with RxJs

--- src/app/projects/email-client/components/auth/auth.guard.ts
+++ src/app/projects/email-client/components/auth/auth.guard.ts
@@ -8,11 +8,28 @@ import { Observable } from 'rxjs';
  * canLoad - User can load this lazily-loaded module and access the routes inside of it.
  */
 
+/**
+ * ## Auth Guard Issues
+ * 
+ * # The Signedin$ behavior subject never gets marked as complete
+ * -> Use some RxJs trickery to mark the behavior subject as complete
+ * 
+ * # If the guard runs before our 'checkAuth' function is done, we will provide the default 'false' value, marking the user as not authenticated
+ * -> Change the default value of the behavior subject to 'null'
+ * 
+ * # If we move the 'checkAuth' call to the guard, we risk not running the function at all if a user only loads another route
+ * -> Leave the 'checkAuth' call in the Email-client Component
+ * 
+ */
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
-  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    return true;
+  canLoad(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> | Promise<boolean> {
+    return new Observable((subscriber) => {
+      subscriber.next(true);
+      //subscriber.complete(); // no longer required to mark.
+    });
   }
 }
