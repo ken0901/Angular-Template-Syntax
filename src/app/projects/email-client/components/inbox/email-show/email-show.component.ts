f9b9a74b2c807891ca62845c5a5fb329735f57c4
ken0901 <ken5260715@gmail.com>
2023-07-27 21:20:04 -0700

    Placeholder markup, extracting URL params

--- src/app/projects/email-client/components/inbox/email-show/email-show.component.ts
+++ src/app/projects/email-client/components/inbox/email-show/email-show.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-email-show',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailShowComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(({ id }) =>{
+      //this.emailService.getEmail(id);
+    });
   }
 
 }
