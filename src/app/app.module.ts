4e2398850cd1767f9241e1a793627b47f25aa23c
ken0901 <ken5260715@gmail.com>
2023-08-02 20:59:24 -0700

    Connecting modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,34 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PasswordGeneratorComponent } from './projects/password-generator/password-generator.component';
-import { CardsComponent } from './projects/cards/cards.component';
 import { CardComponent } from './projects/cards/card/card.component';
-import { TypingChallengeComponent } from './projects/typing-challenge/typing-challenge.component';
-import { PipesComponent } from './projects/pipes/pipes.component';
-import { ConvertPipe } from './projects/pipes/convert.pipe';
-import { DirectivesComponent } from './projects/directives/directives.component';
-import { ClassDirective } from './projects/directives/class.directive';
-import { TimesDirective } from './projects/directives/times.directive';
-import { ModuleProjectComponent } from './projects/module-project/module-project.component';
-import { HomeComponent } from './projects/module-project/components/home/home.component';
-import { NotFoundComponent } from './projects/module-project/components/not-found/not-found.component';
-import { WsearchComponent } from './projects/wsearch/wsearch.component';
-import { SearchModule } from './projects/wsearch/module/search/search.module';
-import { PhotosComponent } from './projects/photos/photos.component';
-import { PhotosModule } from './projects/photos/module/photos.module';
+import { CardsComponent } from './projects/cards/cards.component';
 import { CreditCardComponent } from './projects/credit-card/credit-card.component';
 import { CreditCardModule } from './projects/credit-card/module/credit-card/credit-card.module';
-import { EmailFormComponent } from './projects/email-form/email-form.component';
-import { EmailFormModule } from './projects/email-form/module/email-form/email-form.module';
-import { MathGameComponent } from './projects/math-game/math-game.component';
-import { MathModule } from './projects/math-game/math-module/math.module';
+import { ClassDirective } from './projects/directives/class.directive';
+import { DirectivesComponent } from './projects/directives/directives.component';
+import { TimesDirective } from './projects/directives/times.directive';
 import { EmailClientComponent } from './projects/email-client/email-client.component';
 import { EmailClientModule } from './projects/email-client/module/email-client.module';
-import { SigninComponent } from './projects/email-client/components/auth/signin/signin.component';
-import { SignupComponent } from './projects/email-client/components/auth/signup/signup.component';
-import { LandingComponent } from './projects/landing/landing.component';
+import { EmailFormComponent } from './projects/email-form/email-form.component';
+import { EmailFormModule } from './projects/email-form/module/email-form/email-form.module';
 import { LandingModule } from './projects/landing/module/landing/landing.module';
+import { MathGameComponent } from './projects/math-game/math-game.component';
+import { MathModule } from './projects/math-game/math-module/math.module';
+import { HomeComponent } from './projects/module-project/components/home/home.component';
+import { NotFoundComponent } from './projects/module-project/components/not-found/not-found.component';
+import { ModuleProjectComponent } from './projects/module-project/module-project.component';
+import { PasswordGeneratorComponent } from './projects/password-generator/password-generator.component';
+import { PhotosModule } from './projects/photos/module/photos.module';
+import { PhotosComponent } from './projects/photos/photos.component';
+import { ConvertPipe } from './projects/pipes/convert.pipe';
+import { PipesComponent } from './projects/pipes/pipes.component';
+import { TypingChallengeComponent } from './projects/typing-challenge/typing-challenge.component';
+import { SearchModule } from './projects/wsearch/module/search/search.module';
+import { WsearchComponent } from './projects/wsearch/wsearch.component';
 
 @NgModule({
     declarations: [
@@ -54,7 +51,6 @@ import { LandingModule } from './projects/landing/module/landing/landing.module'
         EmailFormComponent,
         MathGameComponent,
         EmailClientComponent,
-        LandingComponent,
     ],
     imports: [
         BrowserModule,
--- src/app/projects/landing/module/landing/landing.module.ts
+++ src/app/projects/landing/module/landing/landing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherModule } from '../../weather/weather.module';
-
-
+import { LandingComponent } from '../../landing.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LandingComponent],
   imports: [
     CommonModule,
     WeatherModule
+  ],
+  exports:[
+    LandingComponent
   ]
 })
 export class LandingModule { }
