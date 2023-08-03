e2755e3a7f7af6dc143e251437a4f6f8ffcc0924
ken0901 <ken5260715@gmail.com>
2023-08-02 21:18:00 -0700

    Reading the users Location(latitude,longitude)

--- src/app/projects/landing/weather/forecast.service.ts
+++ src/app/projects/landing/weather/forecast.service.ts
@@ -1,5 +1,23 @@
 import { Injectable } from '@angular/core';
 
+// Javascript way - Get latitude and longitude from console log in website develope mode.
+
+// navigator.geolocation.getCurrentPosition(
+//   (position) => console.log(position),
+//   (err) => console.log(err)
+// );
+
+// Javascript way
+
+// getCurrentLocation() {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => {
+//       this.position = position;
+//       getForecaseData();
+//     }
+//   );
+// }
+
 @Injectable({
   providedIn: 'root'
 })
