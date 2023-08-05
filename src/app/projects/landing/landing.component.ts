353f969cea7565ec2ee1eeadf2b2f038662bf4a8
ken0901 <ken5260715@gmail.com>
2023-08-04 20:46:32 -0700

    How to Structure Services

--- src/app/projects/landing/landing.component.ts
+++ src/app/projects/landing/landing.component.ts
@@ -13,3 +13,22 @@ export class LandingComponent implements OnInit {
   }
 
 }
+
+/**
+ *  # Where to place Services
+ *  
+ *  1. Local  - In same module they are used in
+ *     Global - In the module that they most make sense to pair with
+ * 
+ *  2. Local  - In same module they are used in
+ *     Global - In the app module
+ * 
+ *  3. Local  - In same module they are used in
+ *     Global - In a 'core' module
+ * 
+ *  4. Everything  - All services in a 'core' module
+ *     
+ * 
+ *  5. Everything  - All services in the 'app' module
+ *     
+ */
