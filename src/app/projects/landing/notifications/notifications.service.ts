399269ba56c16de9c2c5f3aad2a1e8de6f252bc9
ken0901 <ken5260715@gmail.com>
2023-08-07 20:38:40 -0700

    Building a Command Structure

--- src/app/projects/landing/notifications/notifications.service.ts
+++ src/app/projects/landing/notifications/notifications.service.ts
@@ -17,25 +17,55 @@ import { Subject } from 'rxjs';
  * 
  */
 
+/**
+ *  Success Command                       Error Command                             Clear Command
+ *   key            value              key               value                     key         value
+ *  text    'Fetched 10 articles'      text      'Error fetching weather'          type        'clear'
+ *  type         'success'             type             'error'                     id           2
+ *   id               1                 id                 2
+ * 
+ */
+
+interface Command {
+  id: number;
+  type: 'success' | 'error' | 'clear';
+  text?: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
-  messages: Subject<any>;
+  messages: Subject<Command>;
 
   constructor() {
-    this.messages = new Subject<any>();
+    this.messages = new Subject<Command>();
   }
 
   addSuccess(message: string) {
-    this.messages.next(message);
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'success'
+    });
   }
 
   addError(message: string) {
-    this.messages.next(message);
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'error'
+    });
   }
 
   clearMessage(id: number) {
-    
+    this.messages.next({
+      id,
+      type: 'clear'
+    });
+  }
+
+  private randomId() {
+    return Math.round(Math.random() * 10000);
   }
 }
