4ca86f9738b76fb62197d66faefe9c4d7c53c0f1
ken0901 <ken5260715@gmail.com>
2023-08-08 20:50:35 -0700

    Subject Declarations, Building the HTTP params

--- src/app/projects/landing/news-api/news-api.service.ts
+++ src/app/projects/landing/news-api/news-api.service.ts
@@ -1,4 +1,7 @@
+import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +12,20 @@ export class NewsApiService {
   private apiKey = '';
   private country = 'us';
 
-  constructor() { }
+  pagesInput: Subject<number>;
+  pagesOutput: Observable<any>;
+  numberOfPages: Observable<number>;
+
+  constructor() {
+    this.pagesInput = new Subject();
+    this.pagesOutput = this.pagesInput.pipe(
+      map((page) => {
+        return new HttpParams()
+          .set('apiKey', this.apiKey)
+          .set('country',this.country)
+          .set('pageSize',String(this.pageSize))
+          .set('page', String(page));
+      })
+    );
+   }
 }
