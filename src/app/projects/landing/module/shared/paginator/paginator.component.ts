b8825ada6d6868a21ad3e80573c379d8d86b4f0a
ken0901 <ken5260715@gmail.com>
2023-08-10 21:42:08 -0700

    Paginator Internals, Styling the Current Page

--- src/app/projects/landing/module/shared/paginator/paginator.component.ts
+++ src/app/projects/landing/module/shared/paginator/paginator.component.ts
@@ -8,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class PaginatorComponent implements OnInit {
   //TODO: make sure we receive this value from the parent component
   numberOfPages = 5;
+  currentPage = 1;
 
-  constructor() { }
+  pageOptions: number[];
+
+  constructor() {
+    this.pageOptions = [
+      this.currentPage - 2,
+      this.currentPage - 1,
+      this.currentPage,
+      this.currentPage + 1,
+      this.currentPage + 2,
+    ].filter(pageNumber => pageNumber >= 1 && pageNumber <= this.numberOfPages);
+  }
 
   ngOnInit(): void {
   }
