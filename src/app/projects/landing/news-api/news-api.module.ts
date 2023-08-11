b23ba762027adccae03806382aa5e6bf79d223f2
ken0901 <ken5260715@gmail.com>
2023-08-10 21:15:29 -0700

    Creating Custom Pipes

--- src/app/projects/landing/news-api/news-api.module.ts
+++ src/app/projects/landing/news-api/news-api.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NaArticleListComponent } from './na-article-list/na-article-list.component';
+import { TrimOutletNamePipe } from './trim-outlet-name.pipe';
 
 
 
 @NgModule({
   declarations: [
-    NaArticleListComponent
+    NaArticleListComponent,
+    TrimOutletNamePipe
   ],
   imports: [
     CommonModule
