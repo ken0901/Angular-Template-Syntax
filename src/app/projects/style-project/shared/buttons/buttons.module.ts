839cbd74ed4821a67204b0e5b2afab81737fe404
ken0901 <ken5260715@gmail.com>
2023-08-15 21:53:21 -0700

    Button module

--- src/app/projects/style-project/shared/buttons/buttons.module.ts
+++ src/app/projects/style-project/shared/buttons/buttons.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ButtonModule } from './button/button.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule
+  ],
+  exports: [
+    ButtonModule
   ]
 })
 export class ButtonsModule { }
