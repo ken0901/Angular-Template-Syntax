6fc2c0180439c41f5b6adcf605353b1d45919b0a
ken0901 <ken5260715@gmail.com>
2023-08-21 22:17:54 -0700

    Form Button - form field

--- src/app/projects/style-project/components/pages/shared/shared.component.ts
+++ src/app/projects/style-project/components/pages/shared/shared.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { regex, regexErrors } from '../../../shared';
+import { markFormGroupTouched, regex, regexErrors } from '../../../shared';
 import { ControlItem } from '../../../models/frontend';
 
 @Component({
@@ -83,14 +83,38 @@ export class SharedComponent implements OnInit {
   }
 
   onPatchValue(): void {
-    this.form.patchValue({ input: 'test'});
+    this.form.patchValue({
+        input: 'test',
+        password: 'password',
+        autocomplete: 1,
+        select: 2,
+        checkboxes: [3],
+        radios: 4,
+        date: new Date().getTime(),
+        dateRange: {
+          from: new Date(2023,6,20).getTime(),
+          to: new Date(2022, 1, 18).getTime()
+        }
+    });
   }
 
   onSubmit(): void {
     console.log('Submit !');
+
+    if(!this.form.valid) {
+      markFormGroupTouched(this.form);
+    }
   }
 
   onToggleInline() {
     this.isInline = !this.isInline;
+  }
+
+  onToggleDisable() {
+    if(this.form.enabled){
+      this.form.disable();
+    }else {
+      this.form.enable();
+    }
   }
 }
