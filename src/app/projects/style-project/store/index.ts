15cb220b078fc56507793ecc20fab8da2a04ecd3
ken0901 <ken5260715@gmail.com>
2023-08-23 21:08:18 -0700

    Basic files - using NgRx

---
+++ src/app/projects/style-project/store/index.ts
@@ -0,0 +1,13 @@
+import { ActionReducerMap } from "@ngrx/store";
+
+export interface State {
+    dictionaries: any;
+}
+
+export const reducers: ActionReducerMap<State> = {
+    dictionaries: undefined
+};
+
+export const effects = [
+
+];
