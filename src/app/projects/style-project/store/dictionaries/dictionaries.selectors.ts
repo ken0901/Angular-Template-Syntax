81e6467e866e12fd93b19bee098022aa62d76dba
ken0901 <ken5260715@gmail.com>
2023-08-24 21:09:04 -0700

    Dictionary - selector

---
+++ src/app/projects/style-project/store/dictionaries/dictionaries.selectors.ts
@@ -0,0 +1,40 @@
+import { createSelector, createFeatureSelector } from "@ngrx/store";
+
+import { DictoinariesState } from "./dictionaries.reducer";
+
+export const getDictionariesState = createFeatureSelector<DictoinariesState>('dictionaries');
+
+export const getDictionaries = createSelector(
+    getDictionariesState,
+    (state) => state.entities
+);
+
+export const getLoading = createSelector(
+    getDictionariesState,
+    (state) => state.loading
+);
+
+export const getIsReady = createSelector(
+    getDictionariesState,
+    (state) => state.entities && !state.loading
+);
+
+export const getRoles = createSelector(
+    getDictionaries,
+    (state) => state.roles
+);
+
+export const getQualifications = createSelector(
+    getDictionaries,
+    (state) => state.qualifications
+);
+
+export const getSpecializations = createSelector(
+    getDictionaries,
+    (state) => state.specializations
+);
+
+export const getSkills = createSelector(
+    getDictionaries,
+    (state) => state.skills
+);
--- src/app/projects/style-project/store/dictionaries/index.ts
+++ src/app/projects/style-project/store/dictionaries/index.ts
@@ -2,4 +2,4 @@ export * from './dictionaries.actions';
 export * from './dictionaries.effects';
 export * from './dictionaries.models';
 export * from './dictionaries.reducer';
-// export * from './dictionaries.selectors';
+export * from './dictionaries.selectors';
