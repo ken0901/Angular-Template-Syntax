8d3ebf1ead81c99db0a201a2ce332d2c7f810052
ken0901 <ken5260715@gmail.com>
2023-08-29 21:20:31 -0700

    Redux - Sign out

--- src/app/projects/style-project/components/header/header.component.ts
+++ src/app/projects/style-project/components/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
+  @Input() isAuthorized: boolean;
+  @Output() signOut = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSignOut(): void{
+    this.signOut.emit();
   }
 
 }
--- src/app/projects/style-project/style-project.component.ts
+++ src/app/projects/style-project/style-project.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import * as fromRoot from './store';
 import * as fromDictionaries from './store/dictionaries';
 import * as fromUser from './store/user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'style-project',
@@ -12,15 +13,22 @@ import * as fromUser from './store/user';
   styleUrls: ['./style-project.component.css']
 })
 export class StyleProjectComponent implements OnInit {
+  isAuthorized$: Observable<boolean>;
 
   constructor(private store: Store<fromRoot.State>) { }
 
   ngOnInit(): void {
+    this.isAuthorized$ = this.store.pipe(select(fromUser.getIsAuthorized));
+
     this.store.dispatch(new fromUser.Init);
     this.store.dispatch(new fromDictionaries.Read());
     // this.afs.collection('test').snapshotChanges().subscribe(data => {
     //   console.log(data.map(x => x.payload.doc.data()));
     // });
+  }
+
+  onSignOut(): void {
+    this.store.dispatch(new fromUser.SignOut());
   }
 
 }
