c284f1e63dbb2af6995f3d62142f0d2a63346e63
ken0901 <ken5260715@gmail.com>
2023-08-31 20:26:25 -0700

    File upload - directive

--- src/app/projects/style-project/components/pages/shared/shared.module.ts
+++ src/app/projects/style-project/components/pages/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
-import { ButtonsModule, ControlsModule, IndicatorsModule } from '../../../shared';
+import { ButtonsModule, ControlsModule, IndicatorsModule, PopupsModule } from '../../../shared';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
@@ -17,7 +17,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule,
     ButtonsModule,
     ControlsModule,
-    IndicatorsModule
+    IndicatorsModule,
+    PopupsModule
   ]
 })
 export class SharedModule { }
--- src/app/projects/style-project/shared/popups/files-upload/files-upload.directive.ts
+++ src/app/projects/style-project/shared/popups/files-upload/files-upload.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import { Directive, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { FilesUploadComponent } from './files-upload.component';
 
 @Directive({
   selector: '[appFilesUpload]'
 })
 export class FilesUploadDirective {
+  @Input() multiple: boolean;
+  @Input() crop: boolean;
 
-  constructor() { }
+  @Output() changed = new EventEmitter<string | string[]>();
 
+  constructor(private dialog: MatDialog) { }
+
+  @HostListener('click', ['$event']) onClick(){
+    this.openDialog();
+  }
+
+  private openDialog(): void {
+    const dialogRef = this.dialog.open(FilesUploadComponent, {
+      width: '550px',
+      height: '500px',
+    });
+    
+    dialogRef.afterClosed().subscribe(result => {
+      this.changed.emit(result || null);
+    });
+  }
 }
