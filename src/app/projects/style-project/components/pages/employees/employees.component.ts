3627ab517bc108828da2ac4c9dac97f2dd64399a
ken0901 <ken5260715@gmail.com>
2023-09-14 21:22:14 -0700

    Employees - component and template

--- src/app/projects/style-project/components/pages/employees/employees.component.ts
+++ src/app/projects/style-project/components/pages/employees/employees.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Store, select } from '@ngrx/store';
+import * as fromRoot from '../../../store';
+import * as fromList from './store/list';
+
+import { User } from './store/list/list.models';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.css']
+  styleUrls: ['./employees.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EmployeesComponent implements OnInit {
+  employees$: Observable<User[]>;
 
-  constructor() { }
+  constructor(private store: Store<fromRoot.State>) { }
 
   ngOnInit(): void {
+    this.employees$ = this.store.pipe(select(fromList.getItems));
+
+    this.store.dispatch(new fromList.Read());
   }
 
 }
